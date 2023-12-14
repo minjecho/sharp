3b5cafc2c4e5bfc0e0d905dba495e651fbf96d46
minje <minje@MINJE>
2023-12-14 09:53:19 +0900

    초기 커밋.

---
+++ Study01/Study01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");*/
+            int a = 100;
+            int b = 200;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
