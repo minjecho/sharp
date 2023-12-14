1ef96bb2098af565c02f1ad8f61ceda53588a495
Isaac Cho <add6235@naver.com>
2023-12-14 14:40:10 +0900

    ChatApp01-TCP Socket

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -15,7 +15,7 @@ namespace ChatApp
 
             // TcpListner
             // IP, port
-            TcpListener server = new TcpListener(System.Net.IPAddress.Any, 8080);
+            TcpListener server = new TcpListener(System.Net.IPAddress.Any, 9999);
 
             // Server start
             server.Start();
@@ -23,6 +23,24 @@ namespace ChatApp
             // 연결된 client 받아오기
             // client 접속 전까지 해당 구문에서 stop
             TcpClient client = server.AcceptTcpClient();
+
+            // NetworkStream 객체 생성 : client가 보낸 데이터 저장
+            NetworkStream ns = client.GetStream();
+
+            // Socket : byte[] 형식으로 데이터 통신
+            byte[] data = new byte[1024];
+
+            // client가 write한 데이터 읽어오기
+            ns.Read(data, 0, data.Length);
+
+            // 출력
+            string stringData = Encoding.Default.GetString(data);
+
+            Console.WriteLine(stringData);
+
+            // 서버 종료
+            server.Stop();
+            ns.Close();
         }
     }
 }
---
+++ Study01/Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------- 클라이언트 콘솔창 ----------------------");
+
+            TcpClient client = new TcpClient();
+
+            // IP : 127.0.0.1(자기자신을 나타내는 루프백 IP)
+            // port : 8080
+            try
+            {
+                client.Connect("127.0.0.1", 9999);
+            }
+            catch(Exception err)
+            {
+                string strErr = string.Format("[SYSTEM] : {0}", err.Message);
+                Console.WriteLine(strErr);
+            }
+
+            // '접속합니다' 출력
+            byte[] buf = Encoding.Default.GetBytes("클라이언트 : 접속합니다");
+
+            // Write
+            try
+            {
+                client.GetStream().Write(buf, 0, buf.Length);
+            } catch(Exception err)
+            {
+                string strErr = string.Format("[SYSTEM] : {0}", err.Message);
+                Console.WriteLine(strErr);
+            }
+
+            client.Close();
+        }
+    }
+}
